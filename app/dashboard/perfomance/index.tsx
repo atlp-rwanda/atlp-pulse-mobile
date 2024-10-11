000cbb25b884313bcbb9b3a4002443f23118ed6d
munezeromicha <munezerontaganiramichel@gmail.com>
2024-10-11 11:24:29 +0200

    ft trainee statistics

---
+++ app/dashboard/perfomance/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+import PerformanceScores from '@/components/perfomanceStats';
+export default function TraineeDashboard() {
+  return (
+    <View>
+      <PerformanceScores />
+    </View>
+  );
+}
