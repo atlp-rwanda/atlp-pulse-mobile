22f39382e40b221c05b732e6b78258681b6b0848
PACIFIQUE MBONIMANA <pacifiquemboni@gmail.com>
2024-10-18 13:11:26 +0200

    Fixing sprint rating and comment model (#53)

--- app/dashboard/index.tsx
+++ app/dashboard/index.tsx
@@ -1,13 +1,48 @@
+import React, { PropsWithChildren, useState } from 'react';
 import { StyleSheet, Text, useColorScheme, View } from 'react-native';
-import { PropsWithChildren } from 'react';
 import TraineeRatings from '../../components/sprintRatings';
 import PerformanceScores from '@/components/perfomanceStats';
+import FeedbackModal from '@/components/feedbackModel'; 
+
 export const CustomText = ({ children }: PropsWithChildren) => <Text>{children}</Text>;
+
 const Dashboard = () => {
+ 
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [selectedFeedback, setSelectedFeedback] = useState<any>(null);
+
+ 
+  const openFeedbackModal = (feedback: any) => {
+    setSelectedFeedback(feedback);
+    setIsModalVisible(true);
+  };
+
+  
+  const closeFeedbackModal = () => {
+    setIsModalVisible(false);
+    setSelectedFeedback(null);
+  };
+
+  const colorScheme = useColorScheme();
+  const textColor = colorScheme === 'dark' ? 'text-gray-100' : 'text-gray-800';
+  const bgColor = colorScheme === 'dark' ? 'bg-primary-dark' : 'bg-secondary-light';
+
   return (
-    <View>
+    <View style={{ flex: 1 }}>
+     
       <PerformanceScores />
-      <TraineeRatings />
+
+     
+      <TraineeRatings openFeedbackModal={openFeedbackModal} />
+
+     
+      <FeedbackModal
+        isVisible={isModalVisible}
+        onClose={closeFeedbackModal}
+        feedbacks={selectedFeedback?.feedbacks || []}
+        textColor={textColor}
+        bgColor={bgColor}
+      />
     </View>
   );
 };
