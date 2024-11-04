b6d6763b371f894a7315fdbbabc6354f274171e1
niyibi250 <ericniyibizi1998@gmail.com>
2024-11-04 10:22:17 +0200

    ft : collected UI

--- components/calendar/CalendarBody.tsx
+++ components/calendar/CalendarBody.tsx
@@ -1,18 +1,46 @@
 import { View, Text, TouchableOpacity, useColorScheme } from 'react-native'
-import React, { useState } from 'react'
-import AntDesign from '@expo/vector-icons/AntDesign';
-import CustomButton from './CustomButton'
+import React, { useEffect, useState } from 'react'
 import EventCard from './EventCard';
 import DatePickerCard from './DatePickerCard';
 import WorningCard from './WorningCard';
-import CreateEventCard from './CreateEventCard';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {GET_EVENTS} from '@/graphql/queries/event';
+import { useQuery } from '@apollo/client';
 
+type Event = {
+  id: string;
+  title: string;
+  hostName: string;
+  start: string;
+  end: string;
+  timeToStart: string;
+  timeToEnd: string;
+};
 
 const CalendarBody = () => {
   const [isWarningVisible, setWarningVisible] = useState(false);
-  const [isCreateEventVisible, setCreateEventVisible] = useState(false);
   const [eventToDelete, setEventToDelete] = useState('');
   const colorScheme = useColorScheme();
+  const [authToken, setAuthToken] = useState<string | null>(null);
+
+  const getAuthToken = async () => {
+    try {
+      const token = await AsyncStorage.getItem('authToken');
+      setAuthToken(token);
+    } catch (error) {
+      console.error('Error retrieving token:', error);
+    }
+  };
+
+  useEffect(() => {
+    getAuthToken();
+  }, []);
+  
+  const { loading, error, data } = useQuery(GET_EVENTS, {
+    variables: { authToken },
+    skip: !authToken,
+  });
+
 
   const handleDeleteEvent = (eventName:any) => {
     setEventToDelete(eventName);
@@ -25,32 +53,23 @@ const CalendarBody = () => {
     setWarningVisible(false);
   };
 
-
   return (
     <View className=' justify-center items-center mb-10'>
       <View className='w-full flex-row justify-between items-center px-5 mb-8'>
         <Text className={`text-xl font-Inter-Bold rounded ${colorScheme === 'light' ? 'text-black' : 'text-white'}`}>Calender</Text>
-        <CustomButton
-        title = 'Add Event'
-        handlepress = {()=>setCreateEventVisible(true)}
-        containerstyle = 'bg-violet'
-        textstyle = ' text-white font-Inter-Regular'
-        isloading = {false}
-        />
       </View>
       <View className='w-full'>
         <DatePickerCard/>
       </View>
 
       <View className='w-full'>
+      {data?.getEvents?.map((event: Event) => (
         <EventCard
-        eventName='Event 1' 
+        key={event.id}
+        eventName={event.title}
         onDelete={handleDeleteEvent}
         />
-        <EventCard
-        eventName='Event 1' 
-        onDelete={handleDeleteEvent}
-        />
+      ))}
       </View>
       {isWarningVisible && (
         <View className='absolute flex justify-center items-center bg-opacity-50'>
@@ -63,18 +82,9 @@ const CalendarBody = () => {
         </View>
       )}
 
-      {/* Centering the Create Event Card */}
-      {isCreateEventVisible && (
-        <View className='absolute bg-primary-light flex justify-center items-center bg-opacity-50 rounded-md'>
-          <CreateEventCard
-            isvisible={isCreateEventVisible}
-            onCancel={()=>setCreateEventVisible(false)}
-            onConfirm={handleConfirmDelete}
-          />
-        </View>
-      )}
     </View>
   )
 }
 
 export default CalendarBody
+
