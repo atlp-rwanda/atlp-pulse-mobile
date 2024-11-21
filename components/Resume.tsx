110f5fae432f6ec0f459e98324d10d51951f9266
Bertin M <bertin.m2001@gmail.com>
2024-11-21 12:44:54 +0200

    Fix: resum upload

--- components/Resume.tsx
+++ components/Resume.tsx
@@ -1,14 +1,21 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import { View, Text, TouchableOpacity, ScrollView, useColorScheme, TextInput, ActivityIndicator } from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+  useColorScheme,
+  TextInput,
+  ActivityIndicator,
+} from 'react-native';
 import * as DocumentPicker from 'expo-document-picker';
 import { DocumentPickerResult } from 'expo-document-picker';
-import Button from '@/components/buttons';
 import { UPLOAD_RESUME } from '@/graphql/mutations/Resume.mutations';
 import { useToast } from 'react-native-toast-notifications';
 import { useMutation } from '@apollo/client';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import {jwtDecode} from 'jwt-decode';
+import { jwtDecode } from 'jwt-decode';
 import { GET_TRAINEE_PROFILE } from '@/graphql/queries/user';
 import { useTranslation } from 'react-i18next';
 
@@ -17,7 +24,8 @@ const Resume = () => {
   const textColor = colorScheme === 'dark' ? 'text-gray-100' : 'text-gray-800';
   const bgColor = colorScheme === 'dark' ? 'bg-secondary-dark-900' : 'bg-secondary-light-300';
   const activebuttonbg = 'text-secondary-light bg-action-500';
-  const inactivebuttonbg = colorScheme === 'dark' ? ' bg-secondary-dark-700' : ' bg-secondary-light-800';
+  const inactivebuttonbg =
+    colorScheme === 'dark' ? ' bg-secondary-dark-700' : ' bg-secondary-light-800';
   const [resumeFile, setResumeFile] = useState<DocumentPickerResult | null>(null);
   const [externalLink, setExternalLink] = useState('');
   const [isUploadPC, setIsUploadPC] = useState(true);
@@ -25,12 +33,9 @@ const Resume = () => {
   const [userToken, setUserToken] = useState<string | null>(null);
   const [userId, setUserId] = useState<string | null>(null);
   const toast = useToast();
-  const { t } = useTranslation();
   const [UploadResume, { loading }] = useMutation(UPLOAD_RESUME);
-  const resume_url = process.env.EXPO_PUBLIC_RESUME_URL;
-  const [fileLoading, setFileLoading] = useState(false); 
-
-
+  const [fileLoading, setFileLoading] = useState(false);
+  const { t } = useTranslation();
 
   useEffect(() => {
     const fetchToken = async () => {
@@ -39,11 +44,19 @@ const Resume = () => {
         if (token) {
           setUserToken(token);
         } else {
-          toast.show(t('toasts.dashboard.tokenNotFound'), { type: 'danger', placement: 'top', duration: 3000 });
+          toast.show(t('toasts.dashboard.tokenNotFound'), {
+            type: 'danger',
+            placement: 'top',
+            duration: 3000,
+          });
         }
       } catch (error) {
-        toast.show(t('toasts.dashboard.failedToken'), { type: 'danger', placement: 'top', duration: 3000 });
-      } 
+        toast.show('Failed to retrieve token.', {
+          type: 'danger',
+          placement: 'top',
+          duration: 3000,
+        });
+      }
     };
     fetchToken();
   }, []);
@@ -58,19 +71,23 @@ const Resume = () => {
     if (userToken) {
       try {
         const decoded = jwtDecode<DecodedToken>(userToken as string);
-          setUserId(decoded.userId);
+        setUserId(decoded.userId);
       } catch (error) {
-        toast.show(t('toasts.dashboard.decodeErr'), { type: 'danger', placement: 'top', duration: 3000 });
+        toast.show(t('toasts.dashboard.failedToken'), {
+          type: 'danger',
+          placement: 'top',
+          duration: 3000,
+        });
       }
     }
   }, [userToken]);
 
   const handleFilePick = async () => {
-    setFileLoading(true); 
+    setFileLoading(true);
     try {
       const result = await DocumentPicker.getDocumentAsync({
         type: 'application/pdf',
-      }); 
+      });
       const asset = result.assets?.[0];
       if (!asset?.uri || !asset?.mimeType || !asset?.name) {
         throw new Error('Invalid file selected: Missing required file properties');
@@ -84,25 +101,25 @@ const Resume = () => {
         } as any);
         formData.append('upload_preset', 'mydocs');
         formData.append('resource_type', 'raw');
-
-      const response = await axios.post(
-        RESUME_URL,
-        formData,
-        {
-          headers: {
-            'Content-Type': 'multipart/form-data',
-            'Accept': 'application/json',
-          },
-          timeout: 20000, 
+        // @ts-ignore
+        const response = await axios.post(
+          process.env.RESUME_URL || 'https://api.cloudinary.com/v1_1/dta2axdpw/raw/upload',
+          formData,
+          {
+            headers: {
+              'Content-Type': 'multipart/form-data',
+              Accept: 'application/json',
+            },
+            timeout: 20000,
+          }
+        );
+        if (response.data?.secure_url) {
+          setResumeFile(response.data.secure_url);
+          setResumeName(response.data?.original_filename + '.pdf');
+        } else {
+          throw new Error('Upload successful but secure_url not received');
         }
-      );
-      if (response.data?.secure_url) {
-        setResumeFile(response.data.secure_url);
-        setResumeName(response.data?.original_filename+'.pdf');
-      } else {
-        throw new Error('Upload successful but secure_url not received');
       }
-    }
     } catch (error) {
       if (axios.isAxiosError(error)) {
         console.error('Axios error details:', {
@@ -111,59 +128,66 @@ const Resume = () => {
           response: error.response?.data,
           status: error.response?.status,
         });
-        
       } else {
-        toast.show(t('toasts.dashboard.uploadCvError'), { type: 'danger', placement: 'top', duration: 3000 });
+        toast.show(`Upload error: ${error}`, { type: 'danger', placement: 'top', duration: 3000 });
       }
       throw error;
     }
-    setFileLoading(false); 
+    setFileLoading(false);
   };
   const handleUpload = async () => {
     if (isUploadPC) {
-      if (resumeFile ) {
+      if (resumeFile) {
         await UploadResume({
           context: {
             headers: {
               Authorization: `Bearer ${userToken}`,
             },
-          },        
+          },
           variables: {
             userId: userId,
             resume: resumeFile,
           },
-         update: (cache, { data }) => {
-          try {
-            const existingTraineeProfile: { getProfile: { resume: string } } | null = cache.readQuery({
-              query: GET_TRAINEE_PROFILE,
-            });
+          update: (cache, { data }) => {
+            try {
+              const existingTraineeProfile: { getProfile: { resume: string } } | null =
+                cache.readQuery({
+                  query: GET_TRAINEE_PROFILE,
+                });
 
-            if (existingTraineeProfile) {
-              cache.writeQuery({
-                query: GET_TRAINEE_PROFILE,
-                data: {
-                  getProfile: {
-                    ...existingTraineeProfile.getProfile,
-                    resume: data.uploadResume.resume 
-                  }
-                }
+              if (existingTraineeProfile) {
+                cache.writeQuery({
+                  query: GET_TRAINEE_PROFILE,
+                  data: {
+                    getProfile: {
+                      ...existingTraineeProfile.getProfile,
+                      resume: data.uploadResume.resume,
+                    },
+                  },
+                });
+              }
+            } catch (e) {
+              toast.show('GET_TRAINEE_PROFILE cache update failed.', {
+                type: 'danger',
+                placement: 'top',
+                duration: 3000,
               });
             }
-          } catch (e) {
-            toast.show(t('toasts.dashboard.mutationFail'), { type: 'danger', placement: 'top', duration: 3000 });
-          }
-        },
+          },
           onCompleted: (data) => {
-            toast.show(t('toasts.dashboard.uploadCvSuccess'), { type: 'success', placement: 'top', duration: 3000 });
+            toast.show('Resume uploaded successfully', {
+              type: 'success',
+              placement: 'top',
+              duration: 3000,
+            });
             setResumeName(null);
-          },  
+          },
           onError: (error) => {
             toast.show(error.message, { type: 'danger', placement: 'top', duration: 3000 });
           },
         });
-
       } else {
-        toast.show(t('toasts.dashboard.fileErr'), { type: 'danger', placement: 'top', duration: 3000 });
+        toast.show('Please choose a file', { type: 'danger', placement: 'top', duration: 3000 });
       }
     } else {
       if (externalLink.trim()) {
@@ -172,111 +196,130 @@ const Resume = () => {
             headers: {
               Authorization: `Bearer ${userToken}`,
             },
-          }, 
+          },
           variables: {
             userId: userId,
             resume: externalLink,
           },
           update: (cache, { data }) => {
             try {
-              const existingTraineeProfile: { getProfile: { resume: string } } | null = cache.readQuery({
-                query: GET_TRAINEE_PROFILE,
-              });
-  
+              const existingTraineeProfile: { getProfile: { resume: string } } | null =
+                cache.readQuery({
+                  query: GET_TRAINEE_PROFILE,
+                });
+
               if (existingTraineeProfile) {
                 cache.writeQuery({
                   query: GET_TRAINEE_PROFILE,
                   data: {
                     getProfile: {
                       ...existingTraineeProfile.getProfile,
-                      resume: data.uploadResume.resume 
-                    }
-                  }
+                      resume: data.uploadResume.resume,
+                    },
+                  },
                 });
               }
             } catch (e) {
-                toast.show(t('toasts.dashboard.mutationFail'), { type: 'danger', placement: 'top', duration: 3000 });
+              toast.show('GET_TRAINEE_PROFILE cache update failed.', {
+                type: 'danger',
+                placement: 'top',
+                duration: 3000,
+              });
             }
           },
           onCompleted: (data) => {
-            toast.show(t('toasts.dashboard.uploadCvSuccess'), { type: 'success', placement: 'top', duration: 3000 });
+            toast.show('Resume uploaded successfully', {
+              type: 'success',
+              placement: 'top',
+              duration: 3000,
+            });
             setExternalLink('');
           },
           onError: (error) => {
             toast.show(error.message, { type: 'danger', placement: 'top', duration: 3000 });
           },
-          
         });
       } else {
-        toast.show(t('toasts.dashboard.linkErr'), { type: 'danger', placement: 'top', duration: 3000 });
+        toast.show('Please enter a valid external link', {
+          type: 'danger',
+          placement: 'top',
+          duration: 3000,
+        });
       }
     }
   };
   return (
-    <ScrollView className={`flex bg-gray-900 ${bgColor} p-5`}>
-      <View className="flex-row w-full justify-between mb-4">
+    <ScrollView className={`flex bg-gray-900 ${bgColor} p-10`}>
+      <View className="flex-row justify-between w-full mb-4">
         <TouchableOpacity
-          className={`w-40 p-2 ${isUploadPC ? activebuttonbg : inactivebuttonbg} rounded-md`}
+          className={`p-5 ${isUploadPC ? activebuttonbg : inactivebuttonbg} rounded-md`}
           onPress={() => setIsUploadPC(true)}
         >
-          <Text className="text-white">{t('editProfile.uploadFromPhone')}</Text>
+          <Text className="text-white">Upload from Phone</Text>
         </TouchableOpacity>
         <TouchableOpacity
-          className={`w-40 p-2 ml-2 ${!isUploadPC ? activebuttonbg : inactivebuttonbg} rounded-md`}
+          className={`p-5 ml-2 ${!isUploadPC ? activebuttonbg : inactivebuttonbg} rounded-md`}
           onPress={() => setIsUploadPC(false)}
         >
-          <Text className="text-white">{t('editProfile.addLink')}</Text>
+          <Text className="text-white">Add external link</Text>
         </TouchableOpacity>
       </View>
       {isUploadPC ? (
-        <View className={` w-full p-2  rounded-md  ${colorScheme === 'dark' ? 'text-primary-dark bg-secondary-dark-700' : 'bg-secondary-light-500'}`}>
-          <Text className={`mb-4 ${textColor } text-lg`}>{t('editProfile.uploadResumeFromPhone')}</Text>
-          <View
-            className={` items-start gap-1 `}
-          >
-            <TouchableOpacity className={`p-2 rounded-md mb-4 shadow border-1 border-secondary-dark-700  ${colorScheme === 'dark' ? 'bg-secondary-dark-500' : 'bg-secondary-light-800'}`} onPress={handleFilePick}>
-             <Text className='text-white'> {resumeName && resumeName ? resumeName : 'Choose File'}</Text>
-             {fileLoading && <ActivityIndicator size="small" color="#fff" />} 
+        <View
+          className={` w-full p-2  rounded-md  ${colorScheme === 'dark' ? 'text-primary-dark bg-secondary-dark-700' : 'bg-secondary-light-500'}`}
+        >
+          <Text className={`mb-4 ${textColor} text-lg`}>Upload resume from your Phone</Text>
+          <View className={` items-start gap-1 `}>
+            <TouchableOpacity
+              className={`p-2 rounded-md mb-4 shadow border-1 border-secondary-dark-700  ${colorScheme === 'dark' ? 'bg-secondary-dark-500' : 'bg-secondary-light-800'}`}
+              onPress={handleFilePick}
+            >
+              <Text className="text-white">
+                {' '}
+                {resumeName && resumeName ? resumeName : 'Choose File'}
+              </Text>
+              {fileLoading && <ActivityIndicator size="small" color="#fff" />}
             </TouchableOpacity>
 
             <TouchableOpacity
-            className="bg-action-500  items-center p-2 w-40 rounded-md mb-4 flex-row justify-center"
-            onPress={handleUpload}
-            disabled={loading}
+              className="flex-row items-center justify-center w-40 p-2 mb-4 rounded-md bg-action-500"
+              onPress={handleUpload}
+              disabled={loading}
             >
-            {loading ? (
-              <ActivityIndicator size="small" color="#FFF" />
-            ) : (
-              <Text className={`text-white`}>{t('editProfile.upload')}</Text>
-            )}
+              {loading ? (
+                <ActivityIndicator size="small" color="#FFF" />
+              ) : (
+                <Text className={`text-white`}>Upload</Text>
+              )}
             </TouchableOpacity>
           </View>
         </View>
       ) : (
-        <View className={` items-start gap-5 w-full p-2 ${colorScheme === 'dark' ? ' bg-secondary-dark-700' : 'bg-secondary-light-500'}  rounded-md`}>
-            <Text className={`${textColor} text-lg`}>{t('editProfile.uploadLink')}</Text>
-           
-            <TextInput
+        <View
+          className={` items-start gap-5 w-full p-2 ${colorScheme === 'dark' ? ' bg-secondary-dark-700' : 'bg-secondary-light-500'}  rounded-md`}
+        >
+          <Text className={`${textColor} text-lg`}>Upload resume from external link</Text>
+
+          <TextInput
             className={`${textColor} ${colorScheme === 'dark' ? 'bg-secondary-dark-500' : 'bg-secondary-light-600'}  p-2 w-full rounded-md mb-4"`}
-            placeholder={t('editProfile.enterLink')}
+            placeholder="Enter external link"
             placeholderTextColor="#999"
             value={externalLink}
             onChangeText={setExternalLink}
-            />
+          />
 
-            <TouchableOpacity
-            className="bg-action-500  items-center p-2 w-40 rounded-md mb-4 flex-row justify-center"
+          <TouchableOpacity
+            className="flex-row items-center justify-center w-40 p-2 mb-4 rounded-md bg-action-500"
             onPress={handleUpload}
             disabled={loading}
-            >
+          >
             {loading ? (
               <ActivityIndicator size="small" color="#FFF" />
             ) : (
-              <Text className={`text-white`}>{t('editProfile.upload')}</Text>
+              <Text className={`text-white`}>Upload</Text>
             )}
-            </TouchableOpacity>
-          </View>
-    
+          </TouchableOpacity>
+        </View>
       )}
     </ScrollView>
   );
